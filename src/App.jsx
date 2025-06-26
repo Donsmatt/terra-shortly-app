074eb300fb49d4d639dda1a06f493a1df22aebf5
Vicky041 <osemekev22@gmail.com>
2025-06-26 16:02:44 +0300

    header

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,15 @@
-import Navbar from "./components/Navbar";
-// import { BrowserRouter } from "react-router-dom";
-// import "./styles/index.scss";
+
 import Header from "./components/Header";
-import './styles/variable.scss'
+import './styles/header.scss';
 
 function App() {
   return (
     
       
     <>
-      {/* <Navbar links={[
-          { to: "/features", label: "Features" },
-          { to: "/pricing", label: "Pricing" },
-          { to: "/resources", label: "Resources" },
-        ]}
-        isUser={true}
-      /> */}
+      
       <Header/>
-      <h1>URL Shortener</h1>
+      {/* <h1>URL Shortener</h1> */}
     </>
    
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
-import './header.scss'
+import React, { useState } from 'react'
+import Button from './Button'
+import { AiOutlineCloseSquare } from 'react-icons/ai'
+import { GiHamburgerMenu } from "react-icons/gi";
+
 
 const Header = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+    const menuToggler = () => setMenuOpen(p => !p);
+
   return (
-    <div className='test'>Test</div>
+    <div className='header'>
+        <div className='header__content'>
+            <div>
+                <span className='logo'>Shortly</span>
+            </div>
+            <div>
+                <nav className={`nav ${menuOpen ? "nav--open" : ""}`}>
+                    <a className='nav__item' href={"/"}>Features</a>
+                    <a className='nav__item' href={"/"}>Pricing</a>
+                    <a className='nav__item' href={"/"}>Resources</a>
+                    <div className='nav__button__container'>
+                        <a href={"/"}>
+                            Login
+                        </a> 
+                        <Button label="Sign Up"/>
+                    </div>
+                </nav>
+            </div>
+            <div>
+                <div className='header__button__container'>
+                    <a>Login</a>
+                    <Button label="Sign Up"/>
+                </div>
+                <button className='header__toggler' onClick={menuToggler}>
+                    {!menuOpen ? <GiHamburgerMenu/> : <AiOutlineCloseSquare/>}
+                </button>
+            </div>
+        </div>
+    </div>
   )
 }
 
